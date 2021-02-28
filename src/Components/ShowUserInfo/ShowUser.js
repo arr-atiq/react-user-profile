4e44225b09e04b5707e9eff461791113deaaa84e
aar-atiq <aar.msdeveloper01@gmail.com>
2021-02-28 13:13:15 +0600

    added icon

--- src/Components/ShowUserInfo/ShowUser.js
+++ src/Components/ShowUserInfo/ShowUser.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './ShowUser.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChessQueen, faEnvelope, faFunnelDollar, faPhoneAlt, faUserPlus } from '@fortawesome/free-solid-svg-icons'
 
 const ShowUser = (props) => {
     const {name, email, phone, image, salary} = props.getUser;
     return (
         <div className="show-user">
             <div className="showInfo">
-                <h4>Name:{name}</h4>
-                <h4>Email:{email}</h4>
-                <h4>Phone{phone}</h4>
-                <h4>Salary:{salary}</h4>
-                <button className="main-button">Add Me</button>
+                <h4><FontAwesomeIcon icon={faChessQueen} /> {name}</h4>
+                <h4><FontAwesomeIcon icon={faEnvelope} /> {email}</h4>
+                <h4><FontAwesomeIcon icon={faPhoneAlt} /> {phone}</h4>
+                <h4><FontAwesomeIcon icon={faFunnelDollar} /> {salary}</h4>
+                <button className="main-button"><FontAwesomeIcon icon={faUserPlus} /> Add Me</button>
             </div>
             <div className="imgPart">
                 <img src={image} alt=""/>
