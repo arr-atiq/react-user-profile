f2d4fe13f1d1dff679835715246b448553b05d05
aar-atiq <aar.msdeveloper01@gmail.com>
2021-02-28 22:55:45 +0600

    showed profile picture in cart

--- src/Components/AddCart/Cart.js
+++ src/Components/AddCart/Cart.js
@@ -4,9 +4,11 @@ import './cart.css';
 const Cart = (props) => {
     const myCart = props.addCart;
     let totalSalary = 0;
+    let totalImage = [];
     for (let i = 0; i < myCart.length; i++) {
         const getCart = myCart[i];
         totalSalary = totalSalary + getCart.salary;
+        totalImage =  getCart.image;
         
     }
     return (
@@ -14,6 +16,10 @@ const Cart = (props) => {
             <h2>Added User Summery</h2>
             <h3>Total Added Users: {myCart.length}</h3>
             <h3>Total Salary: ${totalSalary}</h3>
+            <li>
+                <img src={totalImage} alt=""/>
+            </li>
+            
         </div>
     );
 };
